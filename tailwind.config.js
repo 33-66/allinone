d895dd7e7a47bfa3c02f25878b5c64ca3cee1038
33-66 <simonnganga932@gmail.com>
2024-11-19 14:03:05 +0300

    build in  process https://cra.link/deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'true', // or 'media' or false
   theme: {
     extend: {
