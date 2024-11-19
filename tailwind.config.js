7bd0d7c0b02ed70192803e3363800832e3a8102e
33-66 <simonnganga932@gmail.com>
2024-11-19 14:16:55 +0300

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'true', // or 'media' or false
+  darkMode: 'class', // Use 'media' or 'class'
   theme: {
     extend: {
       colors: {
